82c8c83b82fce0c59ce97e33759a81b8d486a8a8
Dennis Grzegorzewski <my_email@email.com>
2016-12-02 00:23:14 -0600

    more comments and cleanup fixed text fixed extra state on search string

--- app/src/main/java/edu/grzegorzewski/customholdingsbuilder/GetHoldingsActivity.java
+++ app/src/main/java/edu/grzegorzewski/customholdingsbuilder/GetHoldingsActivity.java
@@ -19,9 +19,22 @@ import android.widget.TextView;
 
 import edu.grzegorzewski.customholdingsbuilder.services.OclcIntentService;
 
+/**
+ * TODO Class description.
+ *
+ * @author Christopher Montufar.
+ * @version 1.0, 12/01/2016.
+ */
 public class GetHoldingsActivity extends Activity {
 
+    /*
+     * Declare class variables
+     */
     private OclcIntentServiceReceiver receiver;
+
+    /*
+     * Methods.
+     */
 
     /**
      * Executes when activity starts.
@@ -47,37 +60,84 @@ public class GetHoldingsActivity extends Activity {
         Intent msgIntent = new Intent(GetHoldingsActivity.this, OclcIntentService.class);
         msgIntent.putExtra("state", state);
         startService(msgIntent);
-    }
 
+    } // end method onCreate.
+
+    /**
+     *
+     * @since 1.0
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    } //end method onStart.
+
+    /**
+     *
+     * @since 1.0
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+    } //end method onResume.
+
+    /**
+     *
+     * @since 1.0
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
     } //end method onDestroy.
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    /*
+     * Inside classes.
+     */
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
+    /**
+     * TODO Class description.
+     *
+     * @since 1.0
+     */
     public class OclcIntentServiceReceiver extends BroadcastReceiver {
 
+        /*
+         * Declare class variables
+         */
         public static final String PROCESS_RESPONSE = "edu.grzegorzewski.customholdingsbuilder.intent.action.PROCESS_RESPONSE";
 
+        /*
+         * Methods.
+         */
+
+        /**
+         * TODO Method description.
+         *
+         * @param context
+         * @param intent
+         * @since 1.0
+         */
         @Override
         public void onReceive(Context context, Intent intent) {
+
+            // TODO description.
             String processedState = intent.getStringExtra(OclcIntentService.BROADCAST_RESPONSE_STRING);
+
+            // TODO description.
             int textViewId = getResources().getIdentifier(String.format("textView%s", processedState),
                     "id", getPackageName());
+
+            // Find the state label.
             TextView stateLabel = (TextView) findViewById(textViewId);
+
+            // Change the background and text color.
             stateLabel.setBackground(getResources().getDrawable(R.drawable.state_border_on));
             stateLabel.setTextColor(getResources().getColor(R.color.colorPrimaryLight));
             //Toast.makeText(GetHoldingsActivity.this, "State Completed: " + intent.getStringExtra(OclcIntentService.BROADCAST_RESPONSE_STRING), Toast.LENGTH_LONG).show();
-        }
-    }
-}
+
+        } //end method onReceive.
+
+    } //end class OclcIntentServiceReceiver.
+
+} // end class GetHoldingsActivity
